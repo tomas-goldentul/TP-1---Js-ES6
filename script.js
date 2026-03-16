e13c8340ab93f2bb6d71e9f254e49d2f18082c65
TOMAS ARIEL GOLDENTUL <49319223@ort>
2026-03-16 13:17:40 -0300

    Parte 1 completa

---
+++ script.js
@@ -0,0 +1,48 @@
+function FormatearNombre() {
+    let nombre = document.getElementById("NombreReset").value;
+    let Corregido = (nombre.slice(0, 1).toUpperCase()) + (nombre.slice(1).toLowerCase());
+    let Respuesta = document.getElementById("NombreFormateado");
+    Respuesta.innerHTML = Corregido;
+}
+
+function ContarLetras() {
+    let ContarLetras = document.getElementById("ContarLetras").value;
+    let letrasSinEspacios = ContarLetras.replace(" ", "");
+    let cantidadLetras = letrasSinEspacios.length;
+    let Respuesta = document.getElementById("ContadorLetras");
+    Respuesta.innerHTML = cantidadLetras;
+}
+
+function NumeroMayor() {
+    let numeros = document.getElementById("NumeroMayor").value;
+    let listaNumeros = (numeros.split(","));
+    let numeroMayor=0;
+    for (let i = 0; i < listaNumeros.length; i++) {
+        if (listaNumeros[i] > numeroMayor) {
+            numeroMayor = listaNumeros[i];
+        }
+       
+    }
+    console.log("El número mayor es: " + numeroMayor);
+}
+
+function ValidarPassword() {
+    let password = document.getElementById("password").value;
+    if (password.length < 8) {
+        console.log("La contraseña debe tener al menos 8 caracteres.");
+        return false;
+    } else if (/\d/.test(password) === false) {
+        console.log("La contraseña debe contener al menos un número.");
+        return false;
+    } else {
+        console.log("Contraseña válida.");
+        return true;
+    }
+}
+
+function SumarArray(){
+    let array = document.getElementById("array").value;
+    let sum = 0;
+     array.forEach(num => sum += num);
+    console.log("La suma total es: " + sum);
+}
